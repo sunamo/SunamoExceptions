2ae5b9a62fcfc56836b1fa9fe193f72e8516a7c3
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:48:32 +0100

    .

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using SunamoExceptions._sunamo;
+global using SunamoExceptions.InSunamoIsDerivedFrom;
+global using SunamoExceptions.Interfaces;
+global using SunamoExceptions.OnlyInSE;
+global using SunamoExceptions.Attributes;
+global using SunamoValues;
---
+++ OnlyInSE/GlobalUsings.cs
@@ -0,0 +1,14 @@
+
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Collections.Specialized;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.IO;
+global using System.Linq;
+global using System.Net;
+global using System.Reflection;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Xml.Serialization;
