1e3d1d870a74e01a31718fb51669e417f7c3e202
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 13:15:16 +0200

    All buildable, every in separate file

--- GlobalAliasUsing.cs
+++
@@ -1,5 +0,0 @@
-namespace SunamoExceptions;
-
-// už nepoužívám n, protože nepoužívám žádné jiné JSON
-// ale ano, protože FOrmating je v 'Newtonsoft.Json.Formatting' and 'System.Xml.Formatting'
-//global using n = Newtonsoft.Json;
